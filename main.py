4f0e3e2afc5d7cfeda8b253f13edd7027c6c5421
Asil Senel <50266584+asilsenel@users.noreply.github.com>
2025-04-28 19:27:50 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 28 15:57:59 2025
+
+@author: asil.senel
+"""
+
+# main.py
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 28 17:30:00 2025
+
+@author: asil.senel
+"""
+
+from config import PROJECT_FOLDER_PATH, CSV_PATH
+from xaml_module import extract_xaml_activities
+from csv_module import extract_relevant_exceptions
+from matching_module import match_selectors
+
+
+def main():
+    print("🏁 MAIN çalışıyor...")
+
+    # XAML aktivitelerini çek
+    df_xaml = extract_xaml_activities(folder_path=PROJECT_FOLDER_PATH)
+    print(f"✅ XAML DF oluşturuldu: {len(df_xaml)} kayıt.")
+
+    # CSV'den hataları çek
+    df_csv = extract_relevant_exceptions(csv_path=CSV_PATH)
+    print(f"✅ CSV DF oluşturuldu: {len(df_csv)} kayıt.")
+
+    # (Şimdilik sadece DF'leri return ediyoruz, ileride eşleşme işlemi buraya eklenecek.)
+    #return df_xaml, df_csv
+    
+    df_matched = match_selectors(df_xaml, df_csv)
+    return df_matched, df_xaml, df_csv
+
+if __name__ == "__main__":
+    df_matched, df_xaml, df_csv = main()
+
