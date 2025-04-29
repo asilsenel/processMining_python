5aa15dd0f144cee6caf6256ccc8e763f0b6b2108
Asil Senel <50266584+asilsenel@users.noreply.github.com>
2025-04-29 09:02:52 +0300

    Update config_backend.py

--- backend/config_backend.py
+++ backend/config_backend.py
@@ -5,8 +5,8 @@ Created on Tue Apr 29 08:49:35 2025
 @author: asil.senel
 """
 
-# config.py
+# config_backend.py
 
 # Varsayılan klasörler
-PROJECT_ROOT_PATH = "data/projects"
-QUEUE_ROOT_PATH = "data/queue_items"
+PROJECT_ROOT_PATH = "C:/Users/asil.senel/Desktop/Kişisel/ProjectFolders"
+QUEUE_ROOT_PATH = "C:/Users/asil.senel/Desktop/Kişisel/QueueItems"
