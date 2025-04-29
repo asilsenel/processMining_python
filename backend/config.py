626ce6db55871b507b4b4ac948bfb0ef1f2cc60d
Asil Senel <50266584+asilsenel@users.noreply.github.com>
2025-04-29 08:53:40 +0300

    Create config.py
    config for backend

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 29 08:49:35 2025
+
+@author: asil.senel
+"""
+
+# config.py
+
+# Varsayılan klasörler
+PROJECT_ROOT_PATH = "data/projects"
+QUEUE_ROOT_PATH = "data/queue_items"
